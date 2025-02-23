5633ce40b01fce191a91e85c7f6b5b026abe4188
yu-ko-ba <59269129+yu-ko-ba@users.noreply.github.com>
2025-02-23 09:26:28 +0900

    feat(composeApp): implement Compose Multiplatform example app

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/createtypography/example/multiplatform/App.kt
+++ composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/createtypography/example/multiplatform/App.kt
@@ -1,36 +1,30 @@
 package dev.yuyuyuyuyu.createtypography.example.multiplatform
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
+import androidx.compose.ui.text.font.FontFamily
 import createtypography.composeapp.generated.resources.Res
-import createtypography.composeapp.generated.resources.compose_multiplatform
+import createtypography.composeapp.generated.resources.Yomogi_Regular
+import dev.yuyuyuyuyu.createtypography.createTypographyFrom
+import org.jetbrains.compose.resources.Font
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+    MaterialTheme(
+        typography = createTypographyFrom(fontFamily = FontFamily(Font(Res.font.Yomogi_Regular))),
+    ) {
+        Scaffold { innerPadding ->
+            Box(
+                modifier = Modifier.padding(innerPadding),
+            ) {
+                Text(
+                    text = "Hello, ${getPlatform().name}!",
+                )
             }
         }
     }
--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/createtypography/example/multiplatform/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.yuyuyuyuyu.createtypography.example.multiplatform
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
