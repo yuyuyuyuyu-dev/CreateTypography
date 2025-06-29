6bea49495d4923af680d5e6e934bb0ee36238c07
yu <email@yuyuyuyuyu.dev>
2025-06-29 14:02:25 +0900

    publish v0.1.2

--- CreateTypography/build.gradle.kts
+++ CreateTypography/build.gradle.kts
@@ -81,7 +81,7 @@ mavenPublishing {
     coordinates(
         groupId = "dev.yuyuyuyuyu",
         artifactId = "createtypography",
-        version = "0.1.1",
+        version = "0.1.2",
     )
 
     pom {
