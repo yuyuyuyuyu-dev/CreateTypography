6d1066a8f399b44002ad50b81b5ebf3d8fa2da60
yu <yu@yuyuyuyuyu.dev>
2026-02-22 16:18:59 +0900

    feat: migrate to Compose Multiplatform v1.10.0

--- composeApp/src/commonMain/kotlin/dev/yuyuyuyuyu/createtypography/example/multiplatform/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.yuyuyuyuyu.createtypography.example.multiplatform
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
---
+++ library/build.gradle.kts
@@ -0,0 +1,117 @@
+import com.vanniktech.maven.publish.SonatypeHost
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.vanniktech.maven.publish)
+}
+
+kotlin {
+    androidTarget {
+        publishLibraryVariants("release")
+        @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+
+    jvm("desktop")
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    js {
+        browser()
+        binaries.executable()
+    }
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+    }
+
+    sourceSets {
+        androidMain.dependencies {
+            implementation(libs.androidx.compose.ui.text.googlefonts)
+        }
+        commonMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.foundation)
+            implementation(libs.compose.material3)
+            implementation(libs.compose.ui)
+            implementation(libs.compose.components.resources)
+            implementation(libs.compose.uiToolingPreview)
+        }
+    }
+}
+
+android {
+    namespace = "dev.yuyuyuyuyu.createtypography"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    debugImplementation(libs.compose.uiTooling)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates(
+        groupId = "dev.yuyuyuyuyu",
+        artifactId = "createtypography",
+        version = "0.1.3",
+    )
+
+    pom {
+        name = "CreateTypography"
+        description =
+            "A library for Android and Compose Multiplatform, provide functions that create Typography from font name or FontFamily."
+        inceptionYear = "2025"
+        url = "https://github.com/yuyuyuyuyu-dev/CreateTypography#readme"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                name = "yu"
+                url = "https://yuyuyuyuyu.dev"
+            }
+        }
+        scm {
+            url = "https://github.com/yuyuyuyuyu-dev/CreateTypography"
+            connection = "scm:git:git://github.com/yuyuyuyuyu-dev/CreateTypography.git"
+            developerConnection = "scm:git:ssh://git@github.com/yuyuyuyuyu-dev/CreateTypography.git"
+        }
+    }
+}
