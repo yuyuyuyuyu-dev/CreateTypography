f4011d9c3939a26c04120d7bf501474a275ce117
yu <yu@yuyuyuyuyu.dev>
2026-02-23 16:47:24 +0900

    publish v0.2.0

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -85,7 +85,7 @@ mavenPublishing {
     coordinates(
         groupId = "dev.yuyuyuyuyu",
         artifactId = "createtypography",
-        version = "0.1.3",
+        version = "0.2.0",
     )
 
     pom {
